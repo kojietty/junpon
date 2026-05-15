690c33489d3cfb193361a85e6bee4c3d384e2940
kojietty <pagudaruma@gmail.com>
2026-05-15 22:27:39 +0900

    db

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8787',
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     target: 'es2022',
